af1682c08625539f7357e2f0e14e5c7d0dbf14f9
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-05-14 08:36:56 +0200

    Base translation application

---
+++ src/main/java/de/learnlanguage/translation/TranslationApplication.java
@@ -0,0 +1,13 @@
+package de.learnlanguage.translation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/learnlanguage/translation/TranslationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.learnlanguage.translation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
