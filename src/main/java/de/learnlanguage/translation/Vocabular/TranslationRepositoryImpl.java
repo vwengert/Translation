ae23e96e62b36dc3197e7c4a97c240e60efe9879
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-05-16 06:55:42 +0200

    Repository Custom Impl uses configurable bean

--- src/main/java/de/learnlanguage/translation/Vocabular/TranslationRepositoryImpl.java
+++ src/main/java/de/learnlanguage/translation/Vocabular/TranslationRepositoryImpl.java
@@ -1,5 +1,10 @@
 package de.learnlanguage.translation.Vocabular;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -7,9 +12,40 @@ public class TranslationRepositoryImpl implements TranslationRepositoryCustom{
 
     @PersistenceContext
     private EntityManager em;
+    @Autowired
+    private RepoBean rB;
 
     @Override
     public void doSpezialOperation() {
-        System.out.println("Fancy DB Stuff, like stored procedure");
+        rB.getThis();
+    }
+}
+
+interface RepoBean {
+    void getThis();
+}
+
+@ConditionalOnProperty(
+        value="translation.service.implementation",
+        havingValue = "MARIADB")
+@Service
+class RepoBeanMariaImpl implements RepoBean {
+
+    @Override
+    public void getThis() {
+        System.out.println("Version Mariadb");
+    }
+}
+
+@ConditionalOnProperty(
+        value="translation.service.implementation",
+        havingValue = "LIST",
+        matchIfMissing = true)
+@Service
+class RepoBeanGenericImpl implements RepoBean {
+
+    @Override
+    public void getThis() {
+        System.out.println("Version Generic");
     }
 }
