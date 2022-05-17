aed21162310049fb5c8ef1114fa3569239daab3a
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-05-17 19:45:31 +0200

    finish TranslationsServiceListImplTest to cover full Implementation with tests

--- src/test/java/de/learnlanguage/translation/Vocabular/TranslationsServiceListImplTest.java
+++ src/test/java/de/learnlanguage/translation/Vocabular/TranslationsServiceListImplTest.java
@@ -3,6 +3,8 @@ package de.learnlanguage.translation.Vocabular;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -40,10 +42,36 @@ class TranslationsServiceListImplTest {
     }
 
     @Test
+    void addTranslationWillThrowOnTakenWord() {
+        assertThrows(ResponseStatusException.class,
+                () -> translationService.addNewTranslation(new Translation("hello", "taken")));
+    }
+
+    @Test
     void deleteTranslationWillGetReturnListOfOne() {
         translationService.deleteTranslation(1L);
         List<Translation> translationList = translationService.getTranslations();
         assertEquals(1, translationList.size());
+    }
+
+    @Test
+    void deleteTranslationWillThrowWhenIdNotFound() {
+        assertThrows(ResponseStatusException.class,
+                () -> translationService.deleteTranslation(5L));
+    }
+
+    @Test
+    void updateTranslationsAddsNewTranslation() {
+        translationService.updateTranslation(1L, "word", "translation");
+
+        assertEquals("word", translationService.list.get(0).getWord());
+        assertEquals("translation", translationService.list.get(0).getTranslation());
+    }
+
+    @Test
+    void updateTranslationsThrowsWhenTranslationIdIsNotPresent() {
+        assertThrows(ResponseStatusException.class,
+                () -> translationService.updateTranslation(5L, "not", "nicht"));
     }
 
 }
