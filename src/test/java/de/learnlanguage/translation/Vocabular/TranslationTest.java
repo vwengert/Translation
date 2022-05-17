85e0e7e574ee5d67373b7f8d6189cb23b43958f1
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2022-05-17 21:37:44 +0200

    tests for equals and toString for Translation

---
+++ src/test/java/de/learnlanguage/translation/Vocabular/TranslationTest.java
@@ -0,0 +1,37 @@
+package de.learnlanguage.translation.Vocabular;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TranslationTest {
+
+    Translation translation;
+
+    @BeforeEach
+    void setUp() {
+        translation = new Translation(1L, "word", "translation");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Translation{id=1, word='word', translation='translation'}", translation.toString());
+    }
+
+    @Test
+    void testEqualsFalseOnNull() {
+        assertFalse(translation.equals(null));
+    }
+
+    @Test
+    void testEqualsTrueIfObjectEqualsItself() {
+        assertTrue(translation.equals(translation));
+    }
+
+    @Test
+    void testEqualsFalseIfObjectNotTranslationObject() {
+        assertFalse(translation.equals(new String("noTranslationObject")));
+    }
+
+}
